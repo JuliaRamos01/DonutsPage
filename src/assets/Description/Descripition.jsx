7a86b06672cd12ef3578f237228ffc73beab474f
Julia Ramos <julia.jrra@gmail.com>
2023-05-01 11:12:51 -0300

    Estilização da Description e adição do Footer

--- src/assets/Description/Descripition.jsx
+++ src/assets/Description/Descripition.jsx
@@ -1,5 +1,5 @@
 import * as S from "./DescriptionStyle";
-import Donuts from "../images/donutsimg.jpg";
+import Donuts from "../images/lojadonuts.jpg";
 
 export default function Description() {
     return (
@@ -9,10 +9,12 @@ export default function Description() {
             </S.Title>
             <S.TextBox>
                 <img src={Donuts} alt="Donut Rosa" />
-                <p>Mussum Ipsum, cacilds vidis litro abertis. Diuretics paradis num copo é motivis de denguis.Cevadis im ampola pa arma uma pindureta.Suco de cevadiss, é um leite divinis, qui tem lupuliz, matis, aguis e fermentis.Paisis, filhis, espiritis santis.
-
-                    Viva Forevis aptent taciti sociosqu ad litora torquent.Manduma pindureta quium dia nois paga.Leite de capivaris, leite de mula manquis sem cabeça.Atirei o pau no gatis, per gatis num morreus.</p>
+                <p>Mussum Ipsum, cacilds vidis litro abertis. Diuretics paradis num copo é motivis de denguis. Cevadis im ampola pa arma uma pindureta. Suco de cevadiss, é um leite divinis, qui tem lupuliz, matis, aguis e fermentis. Paisis, filhis, espiritis santis.
+                    Viva Forevis aptent taciti sociosqu ad litora torquent. Manduma pindureta quium dia nois paga. Leite de capivaris, leite de mula manquis sem cabeça. Atirei o pau no gatis, per gatis num morreus.</p>
             </S.TextBox>
+            <S.Footer>
+                <h1>Donuts - Feito por Júlia Alves(2023)</h1>
+            </S.Footer>
         </S.DescriptionMain>
     );
 }
--- src/assets/Description/DescriptionStyle.jsx
+++ src/assets/Description/DescriptionStyle.jsx
@@ -3,16 +3,16 @@ import styled from "styled-components";
 export const DescriptionMain = styled.div`
 display: flex;
 flex-direction: column;
-justify-content: space-between;
-border: red 2px solid;
-height: 120vh;
+justify-content: space-around;
+/* border: red 2px solid; */
+height: 190vh;
 width: 100%;
 margin-top: 39rem;
 `;
 
 export const Title = styled.div`
 /* border: green 1px solid; */
-height: 60vh;
+height: 80vh;
 display: flex;
 justify-content: center;
 text-align: center;
@@ -28,15 +28,44 @@ h1{
 
 export const TextBox = styled.div`
 display: flex;
-border: blue 2px solid;
-height: 40vh;
+/* border: blue 2px solid; */
+height: 100vh;
 align-items: center;
+justify-content: space-evenly;
+/* background-color: #3a2755; */
 img{
-    width: 20vw;
-    border-radius: 20px;
-    padding-left: 1rem;
+    width: 23vw;
 }
 p{
+    border: red 1px solid;
+    background: rgba( 58, 39, 85, 0.7 );
+    box-shadow: 0 8px 32px 0 rgba( 31, 38, 135, 0.37 );
+    backdrop-filter: blur( 9.5px );
+    -webkit-backdrop-filter: blur( 9.5px );
+    border-radius: 10px;
+    border: 1px solid rgba( 255, 255, 255, 0.18 );
     font-family: 'Konkhmer Sleokchher', cursive;
+    text-align: justify;
+    width: 50vw;
+    height: 40vh;
+    font-size: 1.2rem;
+    padding: 1rem;
+    color: white;
+    font-weight: lighter;
+}
+`;
+
+export const Footer = styled.div`
+/* border: green 2px solid; */
+height: 25vh;
+background-color: #3a2755;
+display: flex;
+align-items: center;
+justify-content: center;
+h1{
+    color: white;
+    font-family: 'Konkhmer Sleokchher', cursive;
+    font-size: 1.2rem;
+    letter-spacing: 0.1rem;
 }
 `;
